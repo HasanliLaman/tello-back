bee0a8d0cadfa56b2c0fbfba8865326f8a7ce427
“Laman <hesenlileman2001@gmail.com>
2022-10-22 00:59:17 +0400

    Change port

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ mongoose.connect(DB, (err) => {
 
   console.log("MongoDB Connected!");
 
-  app.listen(process.env.PORT, () => {
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => {
     console.log(`Server running on port: ${process.env.PORT}`);
   });
 });
