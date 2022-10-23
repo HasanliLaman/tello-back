bb0ecbcb5fdb1e3593cf3f1795f44a83f02dfcd0
“Laman <hesenlileman2001@gmail.com>
2022-10-23 22:58:37 +0400

    Fix booking controller for multiple elements

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,26 +3,31 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const Product = require("../models/product");
 
 exports.checkout = catchAsync(async (req, res, next) => {
-  const product = await Product.findById(req.params.productId);
+  const cartInfo = [];
+
+  for (const el of req.body.cart) {
+    const product = await Product.findById(el.product);
+    cartInfo.push({ product, quantity: el.quantity });
+  }
+
+  const items = cartInfo.map((el) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: el.product.name,
+        },
+        unit_amount: el.product.price * 100,
+      },
+      quantity: el.quantity,
+    };
+  });
 
   const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: product.name,
-          },
-          unit_amount: product.price * req.body.quantity * 100,
-        },
-        quantity: req.body.quantity,
-      },
-    ],
-
+    line_items: items,
     payment_method_types: ["card"],
     mode: "payment",
     customer_creation: "always",
-    customer_reference: product._id,
     customer_email: req.user.email,
     success_url: `http://localhost:3000/cart`,
     cancel_url: `http://localhost:3000/error`,
