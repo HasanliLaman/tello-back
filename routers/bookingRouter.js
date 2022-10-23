b5de7951be08e6bb8f79094f5d1e277e98dcf1cd
“Laman <hesenlileman2001@gmail.com>
2022-10-23 23:05:47 +0400

    Change checkout route

--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -2,6 +2,6 @@ const bookingController = require("../controllers/bookingController");
 const router = require("express").Router();
 const protectAuth = require("../middleware/protectAuth");
 
-router.post("/checkout/:productId", protectAuth, bookingController.checkout);
+router.post("/checkout", protectAuth, bookingController.checkout);
 
 module.exports = router;
