21a875d79d22cbb781e1b6c7aeef4825bef1e49a
“Laman <hesenlileman2001@gmail.com>
2022-10-27 08:33:49 +0400

    Add route for stripe webhook

--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const protectAuth = require("../middleware/protectAuth");
 
 router.post("/checkout", protectAuth, bookingController.checkout);
+router.post("/webhook", bookingController.webhook);
 
 module.exports = router;
